a0c192ef8e7de1be71b83f9a6aa7457e89b4b512
NivereQ <urfoxiq@gmail.com>
2025-01-21 14:13:17 +0100

    restored

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,8 @@ const {
 const getCurrentMs = () => {
    return Date.now();
 }
-let lastTime = Cookies.get("ms") || null;
 let time = getCurrentMs();
 log(`⌛️ | nivereq.pl is loading...`);
 document.body.onload = () => {
-   let loadTime = getCurrentMs() - time;
-   log(`✅ | nivereq.pl loaded successfully in ${loadTime}ms.`);
-   loadTime > lastTime && log(`⚠️ | last loading time is shorten`);
-   Cookies.set("ms", loadTime);
+   log(`✅ | nivereq.pl loaded successfully in ${getCurrentMs() - time}ms.`);
 }
