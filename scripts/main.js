d47a7c31a881e930e90d1567de1f103a13cb09ae
NivereQ <urfoxiq@gmail.com>
2025-01-21 12:15:37 +0100

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/*
+Copyright by NivereQ 2025
+
+*/
+const {
+   log
+} = console; // Easy logging.
+const getCurrentMs = () => {
+   return Date.now();
+}
+let time = getCurrentMs();
+log(`⌛️ | nivereq.pl is loading...`);
+document.body.onload = () => {
+   log(`✅ | nivereq.pl loaded successfully in ${getCurrentMs() - time}ms.`);
+}
