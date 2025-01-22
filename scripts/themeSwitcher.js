f77add2ceacf6ad50c973956173c04a2d175e93b
NivereQ <urfoxiq@gmail.com>
2025-01-22 09:52:38 +0100

    Update themeSwitcher.js

--- scripts/themeSwitcher.js
+++ scripts/themeSwitcher.js
@@ -1,17 +1,22 @@
-const switchTheme = () => {
-  let darkMode = getCookie("dark")=="true" ? true : setCookie("dark","false", 30).value;
-  if(darkMode) {
+function switchTheme(){
+  if(getCookie("dark")){
+    let darkMode = getCookie("dark");
+  }
+  else{
+    let darkMode = setCookie("dark","false",30).value;
+  }
+  if(darkMode == "true") {
     document.querySelectorAll("*").forEach(el => el.classList.add("theme-dark"));
   }
     document.querySelectorAll("*").forEach(el => {
-      if(el.classList.contains("theme-dark") {
+      if(el.classList.contains("theme-dark")) {
         el.classList.remove("theme-dark");
-        setCookie("dark", false, 30);
+        setCookie("dark", "false", 30);
         darkMode = false;
       }
       else{
         el.classList.add("theme-dark");
-        setCookie("dark", true, 30);
+        setCookie("dark", "true", 30);
         darkMode = true;
       }
     return darkMode;
