9554420d64579da97530e8c807ecc1ed94916d39
NivereQ <urfoxiq@gmail.com>
2025-01-22 19:21:34 +0100

    Compressed

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,1 @@
-/*
-Copyright by NivereQ 2025
-
-*/
-const {
-   log
-} = console; // Easy logging.
-const getCurrentMs = () => {
-   return Date.now();
-}
-let time = getCurrentMs();
-log(`⌛️ | nivereq.pl is loading...`);
-document.body.onload = () => {
-   themeInit();
-   log(`✅ | nivereq.pl loaded successfully in ${getCurrentMs() - time}ms.`);
-}
+const{log:log}=console,getCurrentMs=()=>Date.now();let time=getCurrentMs();log("⌛️ | nivereq.pl is loading..."),document.body.onload=()=>{themeInit(),log(`✅ | nivereq.pl loaded successfully in ${getCurrentMs()-time}ms.`)};
