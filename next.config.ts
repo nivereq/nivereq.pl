ace893165eea9dcc3adccf0569c9b3503cead298
nivereq <kontakt@nivereq.pl>
2025-04-15 08:57:26 +0200

    Messing aroud to make it working with cloudflare pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://cdn.discordapp.com/**')],
   },
 };
  
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
