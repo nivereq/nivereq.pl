4a5079fada11c3b3eb772b98b84ad5e21a2d5bbc
nivereq <kontakt@nivereq.pl>
2025-05-03 00:27:04 +0200

    Update Date.ts

--- src/components/Date.ts
+++ src/components/Date.ts
@@ -2,5 +2,5 @@ export function Age() {
     let bdayMs = new Date("2008-05-03").getTime();
     let currentMs = new Date().getTime();
     let diff = currentMs - bdayMs;
-    return Math.floor((diff / (1000 * 3600 * 24 * 365.25)));
+    return Math.floor((diff / (1000 * 3600 * 24 * 365)));
 }
