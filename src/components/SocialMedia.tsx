b9e55aba0551d32314a772355f264c46d392a54d
nivereq <urfoxiq@gmail.com>
2025-09-07 13:47:34 +0200

    fixes

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,21 +2,23 @@ export default function SocialMedia() {
 
 return (
     <>
-        <span className="social">
-            <a target="_blank" title="Discord" href="https://discord.com/invite/rVQW6sHfAv"><i className="bi bi-discord"></i></a>
-        </span>
-        <span className="social">
-            <a target="_blank" title="YouTube" href="https://www.youtube.com/@urnivereq"><i className="bi bi-youtube"></i></a>
-        </span>
-        <span className="social">
-            <a target="_blank" title="TikTok" href="https://www.tiktok.com/@nivereq"><i className="bi bi-tiktok"></i></a>
-        </span>
-        <span className="social">
-            <a target="_blank" title="X/Twitter" href="https://x.com/@nivereq"><i className="bi bi-twitter-x"></i></a>
-        </span>
-        <span className="social">
-            <a target="_blank" title="GitHub" href="https://github.com/nivereq"><i className="bi bi-github"></i></a>
-        </span>
+        <div>
+            <span className="social">
+                <a target="_blank" title="Discord" href="https://discord.com/invite/rVQW6sHfAv"><i className="bi bi-discord"></i></a>
+            </span>
+            <span className="social">
+                <a target="_blank" title="YouTube" href="https://www.youtube.com/@nivuss7"><i className="bi bi-youtube"></i></a>
+            </span>
+            <span className="social">
+                <a target="_blank" title="TikTok" href="https://www.tiktok.com/@nivereq"><i className="bi bi-tiktok"></i></a>
+            </span>
+            {/* <span className="social">
+                <a target="_blank" title="X/Twitter" href="https://x.com/@nivereq"><i className="bi bi-twitter-x"></i></a>
+            </span> */}
+            <span className="social">
+                <a target="_blank" title="GitHub" href="https://github.com/nivereq"><i className="bi bi-github"></i></a>
+            </span>
+        </div>
     </>
 )
 }
